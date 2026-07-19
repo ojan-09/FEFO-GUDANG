d7d3f49ea3d8f8d04fd5acf4a634ac0632faf451
ojan <0906fauzan@gmail.com>
2026-07-19 14:53:53 +0700

    chore: hapus file skrip uji coba

--- debug_roles.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'public/index.php';
-$authorize = service('authorization');
-$authenticate = service('authentication');
-echo "User ID: " . $authenticate->id() . "\n";
-print_r($authorize->getGroupsForUser($authenticate->id()));
